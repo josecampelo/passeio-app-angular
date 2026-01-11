ec9ad4387e38106a222b780e7a7d8c47d0860c87
José Campelo <josecampelojr@gmail.com>
2026-01-11 07:10:35 -0300

    feat: implementa o serviço de lugares com os métodos de listar e salvar

--- src/app/pages/lugares/services/lugar.service.ts
+++ src/app/pages/lugares/services/lugar.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Lugar } from '../models/lugar';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LugarService {
+  private http = inject(HttpClient);
+  private readonly API = 'http://localhost:3000/lugares';
 
-  constructor() { }
+  listar(): Observable<Lugar[]> {
+    return this.http.get<Lugar[]>(this.API);
+  }
+
+  salvar(lugar: Lugar): Observable<Lugar> {
+    return this.http.post<Lugar>(this.API, lugar);
+  }
 }
