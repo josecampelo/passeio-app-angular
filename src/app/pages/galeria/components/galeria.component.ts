3c456ae1a8c58d35fac60918ce4d9a931ba7663f
José Campelo <josecampelojr@gmail.com>
2026-01-11 11:49:32 -0300

    feat: implementa busca por nome ou local em galeria

--- src/app/pages/galeria/components/galeria.component.ts
+++ src/app/pages/galeria/components/galeria.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, inject } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { LugarService } from '../../../core/services/lugar.service';
 import { Lugar } from '../../../core/models/lugar'; 
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-galeria',
@@ -12,11 +14,34 @@ export class GaleriaComponent implements OnInit{
   private lugarService = inject(LugarService);
 
   public lugares: Lugar[] = [];
+  public lugaresFiltrados: Lugar[] = [];
+  public inputBusca = new FormControl('');
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
       this.lugarService.listar().subscribe({
-        next: (dados) => this.lugares = dados,
+        next: (dados) => {
+          this.lugares = dados;
+          this.lugaresFiltrados = dados;
+        },
         error: (err) => console.error('Erro ao carregar lugares:', err)
     });
+
+    this.inputBusca.valueChanges
+      .pipe(
+        debounceTime(500),
+        distinctUntilChanged()
+      )
+      .subscribe(valor => {
+        this.filtrarLugares(valor || '');
+      });
+  }
+
+  public filtrarLugares(termo: string) {
+    const busca = termo.toLowerCase();
+    
+    this.lugaresFiltrados = this.lugares.filter(lugar => 
+      lugar.nome.toLowerCase().includes(busca) || 
+      lugar.localizacao.toLowerCase().includes(busca)
+    );
   }
 }
--- src/app/pages/galeria/galeria.module.ts
+++ src/app/pages/galeria/galeria.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { GaleriaRoutingModule } from './galeria-routing.module';
 import { GaleriaComponent } from './components/galeria.component';
 import { SharedModule } from '../../shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { SharedModule } from '../../shared/shared.module';
   imports: [
     CommonModule,
     GaleriaRoutingModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule
   ]
 })
 export class GaleriaModule { }
