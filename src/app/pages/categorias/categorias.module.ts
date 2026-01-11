62fd38c4c9b9b73c9e54e373124e786ca2d4f41a
José Campelo <josecampelojr@gmail.com>
2026-01-11 06:50:19 -0300

    feat: implementa lógica do componente de categoria com formulário reativo
    feat: implementa cadastro de categorias com formulário reativo e redirecionamento para home

--- src/app/pages/categorias/categorias.module.ts
+++ src/app/pages/categorias/categorias.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+
 import { CategoriasRoutingModule } from './categorias-routing.module';
 import { CategoriaFormComponent } from './components/categoria-form/categoria-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +13,8 @@ import { CategoriaFormComponent } from './components/categoria-form/categoria-fo
   ],
   imports: [
     CommonModule,
-    CategoriasRoutingModule
+    CategoriasRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class CategoriasModule { }
--- src/app/pages/categorias/components/categoria-form/categoria-form.component.ts
+++ src/app/pages/categorias/components/categoria-form/categoria-form.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CategoriaService } from '../../services/categoria.service';
+import { Categoria } from '../../models/categoria';
 
 @Component({
   selector: 'app-categoria-form',
@@ -6,6 +10,44 @@ import { Component } from '@angular/core';
   templateUrl: './categoria-form.component.html',
   styleUrl: './categoria-form.component.scss'
 })
-export class CategoriaFormComponent {
+export class CategoriaFormComponent implements OnInit {
+  private formBuilder = inject(FormBuilder);
+  private categoriaService = inject(CategoriaService);
+  private router = inject(Router);
 
+  public formularioCategoria!: FormGroup;
+
+  public ngOnInit(): void {
+      this.criarFormulario();
+  }
+
+  private criarFormulario(): void {
+    this.formularioCategoria = this.formBuilder.group({
+      nome: ['', [Validators.required, Validators.minLength(3)]],
+      descricao: ['', [Validators.required, Validators.maxLength(200)]]
+    });
+  }
+
+  public voltar(): void {
+    this.router.navigate(['/']);
+  }
+
+  public salvarFormulario(): void {
+    if (this.formularioCategoria.valid) {
+      const novaCategoria: Categoria = this.formularioCategoria.value;
+
+      this.categoriaService.salvar(novaCategoria).subscribe({
+        next: (res) => {
+          alert('Categoria cadastra com sucesso!');
+          this.voltar();
+        },
+        error: (err) => {
+          console.error('Erro ao salvar categoria:', err);
+          alert('Ocorreu um erro ao salvar. Verifique se o json-server está rodando.');
+        }
+      })
+    } else {
+      this.formularioCategoria.markAllAsTouched();
+    }
+  }
 }
